16dee34a89a91d01a8b898c4715dc98b74f19d07
Lorraine Warren <lorrywarren@gmail.com>
2025-05-02 12:58:42 +0100

    Add project files.

---
+++ Basic_Calculator/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic_Calculator
+{
+    class Program
+    {
+        // Set variables to store user input
+        var number1;
+        var number2;
+        static void Main(string[] args)
+        {
+            // 
+            Console.WriteLine("Welcome to the calculator");
+
+            
+        }
+    }
+}
