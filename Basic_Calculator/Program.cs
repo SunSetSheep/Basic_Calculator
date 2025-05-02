e0398533f54bdf8bbd20605e6ff00b47146bd00f
Lorraine Warren <lorrywarren@gmail.com>
2025-05-02 13:18:26 +0100

    Initialize the second user inputted number, also converted string to int.

--- Basic_Calculator/Program.cs
+++ Basic_Calculator/Program.cs
@@ -16,13 +16,26 @@ namespace Basic_Calculator
             int number2;
 
             // Message to display to user on start
-            Console.WriteLine("Welcome to the calculator");
+            Console.WriteLine("Welcome to the calculator ! \n");
 
             // Tell user to insert a number 
-            Console.WriteLine("Please enter your first number.");
+            Console.WriteLine("Please enter your first number \n ");
             
             // Hold user response in varialbe and convert string to int
             number1 = Convert.ToInt32(Console.ReadLine());
+
+            // Tell user to insert another number
+            Console.WriteLine("Please enter your second number \n ");
+
+            // Hold user second response in variable and convert string to int
+
+            number2 = Convert.ToInt32(Console.ReadLine());
+
+            // Handle operation to numbers
+
+            
+
+
 
             // Key console open
             Console.ReadKey();
