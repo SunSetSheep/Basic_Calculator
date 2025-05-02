d0f269450cfa54cc03e696f48153d02dccbc2d93
Lorraine Warren <lorrywarren@gmail.com>
2025-05-02 13:08:40 +0100

    Update Variable types to int, insert an area for user first inputted number to be stored and converted from string to type int.

--- Basic_Calculator/Program.cs
+++ Basic_Calculator/Program.cs
@@ -8,13 +8,26 @@ namespace Basic_Calculator
 {
     class Program
     {
-        // Set variables to store user input
-        var number1;
-        var number2;
+        
         static void Main(string[] args)
         {
-            // 
+            // Set variables to store user input
+            int number1;
+            int number2;
+
+            // Message to display to user on start
             Console.WriteLine("Welcome to the calculator");
+
+            // Tell user to insert a number 
+            Console.WriteLine("Please enter your first number.");
+            
+            // Hold user response in varialbe and convert string to int
+            number1 = Convert.ToInt32(Console.ReadLine());
+
+            // Key console open
+            Console.ReadKey();
+
+
 
             
         }
